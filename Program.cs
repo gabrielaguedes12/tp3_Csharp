17fd44375e7bce36a52d3f71a1a61fb9156e8dc4
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-11-30 20:02:02 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace tp3_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classe:");
+            Console.WriteLine("Objeto:");
+            Console.WriteLine("Campos ou Atributos:");
+            Console.WriteLine("Métodos no contexto da POO:");
+
+        }
+    }
+}
